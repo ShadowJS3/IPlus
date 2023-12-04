1970daf4cd7b9cf6554713cdf36062a18d968c2f
nicky48 <nickyuri48@proton.me>
2023-12-04 17:45:36 -0300

    change tailwind config file to ts and remove index.html from content

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import { Config } from "tailwindcss";
+
+const tailwindConfig: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default tailwindConfig;
